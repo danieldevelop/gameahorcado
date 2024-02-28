4257a628a667c9cf9e85268254099b6174a5dc2b
Daniel Gomez <dgomez.informatica@gmail.com>
2024-02-28 18:30:45 -0300

    lista de palabras secretas

---
+++ assets/js/words.js
@@ -0,0 +1,13 @@
+export const palabras = [
+    'ALURA', 
+    'AHORCADO', 
+    'ORACLE', 
+    'HTML', 
+    'CSS', 
+    'JAVASCRIPT',
+    'REACT',
+    'ANGULAR',
+    'JAVA',
+    'PYTHON',
+    'GO'
+];
